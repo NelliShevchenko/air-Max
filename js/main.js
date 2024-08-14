2ca00b517b7914bc77f46dec8367875603f70d9b
NelliShevchenko <gho020507@gmail.com>
2024-08-14 22:05:57 +0300

    class

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const chooseColor = document.guerySelectorAll('.choose__color-btn');
+const contentItem = document.guerySelectorAll('.content-item');
+
+
+chooseColor.forEach(function(element){
+    element.addEventListener('click', open)
+});
+
+function open(evt){
+    
+}
